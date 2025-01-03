37cb19568afbd6599e78c569e155bb351a2069e2
Pawan Solanki <pawansolanki0701@gmail.com>
2025-01-04 00:34:56 +0530

    __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -27,9 +27,9 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'dummyformyweb@gmail.com')
-    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'mkia kjxp eucf nfpn')
-    app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'dummyformyweb@gmail.com')
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'your-email@gmail.com')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'your-key')
+    app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', 'your-email@gmail.com')
 
     mail.init_app(app)
 
